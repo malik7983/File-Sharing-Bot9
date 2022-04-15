45b17d6ed0eeec0e1177def567f2b8b38e6a17af
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-04-16 03:58:28 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -185,7 +185,7 @@ async def index_files_to_db(lst_msg_id, chat, msg, bot):
                     else:
                         logger.exception(e)
                 current += 1
-                if current % 40 == 2:
+                if current % 40 == 40:
                     can = [[InlineKeyboardButton('Cancel', callback_data='index_cancel')]]
                     reply = InlineKeyboardMarkup(can)
                     await msg.edit_text(
