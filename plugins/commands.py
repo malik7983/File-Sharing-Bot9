586fafc4178083c4d438fb00b8154e3413fde4a0
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-04-15 21:13:16 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -18,7 +18,7 @@ async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
             [
-                InlineKeyboardButton('𝙿𝚁𝙸𝚅𝙰𝚃𝙴 𝙱𝙾𝚃', url=f"https://t.me/m_house786")
+                InlineKeyboardButton('😎 contact me 😎', url=f"https://t.me/sahid_malik")
             ]
             ]
         reply_markup = InlineKeyboardMarkup(buttons)
