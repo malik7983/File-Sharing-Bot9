39b4a8628e69b56e9ff51deb8cb9942a838edeee
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-04-15 20:02:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Aadhi000/Ajax.git /Ajax
+  git clone https://github.com/malik7983/File-Sharing-Bot9.git /File-Sharing-Bot9
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Ajax
+  git clone $UPSTREAM_REPO /File-Sharing-Bot9
 fi
-cd /Ajax
+cd /File-Sharing-Bot9
 pip3 install -U -r requirements.txt
 echo "Starting ᗩᒍᗩ᙭....🔥"
 python3 bot.py
